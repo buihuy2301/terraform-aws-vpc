cdbb551df1493ea7bd77e02a062383789a324032
Huy BQ <huybq@Huys-MacBook-Pro.local>
2023-12-08 13:49:34 +0700

    add tf & git ignore file

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+
+  access_key = "mock_access_key"
+  secret_key = "mock_secret_key"
+  region     = "us-east-2"
+
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+  }
+}
+
+module "vpc" {
+  source = "./vpc"
+
+  vpc_cidr_block    = "10.0.0.0/16"
+  private_subnet    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnet     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  availability_zone = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
+
